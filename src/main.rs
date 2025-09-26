1e8ea0698d681f32e18cd8aef457abc947805e64
TheBookwyrms <60119564+TheBookwyrms@users.noreply.github.com>
2025-09-26 17:33:19 +0200

    Use Render Default

--- src/main.rs
+++ src/main.rs
@@ -35,16 +35,17 @@ fn main() {
 
     
 
-    let mut window = init_window_and_opengl();
-    let mut camera = Camera::new();
-    let mut lighting = Lighting::new();
-    let program_holder = ProgramHolder::new(
-        &window.opengl,
-        [ProgramType::SimpleOrthographic, ProgramType::BlinnPhongOrthographic]
-    );
+    //let mut window = init_window_and_opengl();
+    //let mut camera = Camera::new();
+    //let mut lighting = Lighting::new();
+    //let program_holder = ProgramHolder::new(
+        //&window.opengl,
+        //[ProgramType::SimpleOrthographic, ProgramType::BlinnPhongOrthographic]
+    //);
 
 
-    let mut render = Render::new(window, camera, lighting, program_holder);
+    let mut render = Render::Default::default()
+    //let mut render = Render::new(window, camera, lighting, program_holder);
     render.setup_render();
 
 
