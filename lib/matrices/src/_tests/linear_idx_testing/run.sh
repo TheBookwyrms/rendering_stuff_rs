f00d73e96386c4c8f5582b3b412b34fb5279bd7c
TheBookwyrms <60119564+TheBookwyrms@users.noreply.github.com>
2025-10-12 23:30:29 -0400

    some cleanup

--- lib/matrices/src/_tests/linear_idx_testing/run.sh
+++
@@ -1,7 +0,0 @@
-rustc full_test.rs
-if [ "$?" == "0" ]; then
-    echo "test"
-    ./full_test
-else
-    echo "compilation failed"
-fi
--- lib/matrices/src/_tests/mod.rs
+++
@@ -1,5 +0,0 @@
-mod vector;
-//mod matrix_arr_not_vec;
-pub mod matrix_as_1_array;
-//pub use vector;
-pub mod matrix_with_types;
